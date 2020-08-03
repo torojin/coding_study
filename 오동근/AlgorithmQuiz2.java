657a597ecd75ef1372c4eea3809423e37668892c
dkpoint <dkpoint@naver.com>
2020-08-03 15:52:33 +0900

    2020-08-03 1552
    2020-08-03 1552

---
+++ 오동근/AlgorithmQuiz2.java
@@ -0,0 +1,32 @@
+package com.dkpoint.algorithm.quiz;
+
+import java.util.Scanner;
+
+/*
+2.
+안녕하세요 를 5번 출력 하자
+결과)
+안녕하세요
+안녕하세요
+안녕하세요
+안녕하세요
+안녕하세요
+*/
+
+public class AlgorithmQuiz2 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("문제-2");
+		
+		String message = "안녕하세요";
+		
+		int count = 0;
+		while(count < 5) {
+			count++;
+			System.out.println(message);
+		}
+		
+	}
+	
+}
