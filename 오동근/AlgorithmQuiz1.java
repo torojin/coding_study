39fb7cf055de72c158d538df6b0d40a0ada1e6d2
dkpoint <dkpoint@naver.com>
2020-08-06 21:25:50 +0900

    2020-08-06 2125
    2020-08-06 2125

--- 오동근/AlgorithmQuiz1.java
+++ 오동근/AlgorithmQuiz1.java
@@ -1,13 +1,11 @@
 package com.dkpoint.algorithm.quiz;
 
-import java.util.Scanner;
-
 /*
 1.
 임의의 시험 점수를 입력(혹은 랜덤으로)받아 
 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 
 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
-*/
+ */
 
 public class AlgorithmQuiz1 {
 	
