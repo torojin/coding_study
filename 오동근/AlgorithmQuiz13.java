2faf20c16ac3e167c1e311330d0f65420f081be1
dkpoint <dkpoint@naver.com>
2020-08-11 18:22:23 +0900

    2020-08-11 1822
    2020-08-11 1822

---
+++ 오동근/AlgorithmQuiz13.java
@@ -0,0 +1,30 @@
+package com.dkpoint.algorithm.quiz;
+
+/*
+13.
+1+(1+2)+(1+2+3)+(1+2+3+4)+...+(1+2+3+...+10)의 결과를 계산하시오.
+*/
+
+public class AlgorithmQuiz13 {
+
+	public static void main(String[] args) {
+		
+		int number;
+		int number_range = 10;
+		int result = 0;
+		
+		for(number = 1; number <= number_range; number++) {
+			
+			for(int sum = 1; sum <= number; sum++) {
+			
+				result += sum;
+				
+			}
+			
+		}
+		
+		System.out.println(result);
+		
+	}
+	
+}
