ea36d8810c83e46a441310aaa8a1ae3ad732aa45
kirimaki <kirimaki1@nate.com>
2020-08-19 03:39:49 +0900

    20200819_0339

---
+++ 김주연/Test21.java
@@ -0,0 +1,34 @@
+public class Test21 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//@author KJY
+		
+		//21. 1~10000 사이에 8이 몇번 나오는가?
+		
+		int count = 0;
+		int num = 10000;
+		/*
+		
+		8 18 28 38 48 58 68 78 80 81 82 83 84 85 86 87 88 88 89 98 
+		108 118 128 138 148 158 168 178 188 198
+		808 818 828 838 848 858 868 878 888 898
+		908 918 928 938 948 958 968 978 988 998
+		1008
+		8888
+		*/
+		
+		for(int i = 1; i <= num; i++) {
+			
+			
+			
+		}
+			
+		
+		
+		System.out.println(count);
+		
+	}
+
+}
