6184d0d2f6943ac86120ab8ccf0ebac4fb0d8efe
dkpoint <dkpoint@naver.com>
2020-08-26 12:29:07 +0900

    2020-08-26 1229
    2020-08-26 1229

---
+++ 오동근/AlgorithmQuiz26.java
@@ -0,0 +1,23 @@
+package com.dkpoint.algorithm.quiz;
+
+/*
+26.
+"hello world" 를 꺼구로 출력하세요
+결과:
+dlrow olleh
+*/
+
+public class AlgorithmQuiz26 {
+
+	public static void main(String[] args) {
+
+		String string_vlaue = "hello world";
+		int string_length = string_vlaue.length();
+
+		for (int index = string_length - 1; index >= 0; index--) {
+			System.out.print(string_vlaue.charAt(index));
+		}
+
+	}
+
+}
